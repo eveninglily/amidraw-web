d2618db32308f4fdec94687f9c0421b38ab4c7c7
Evan McIntire <mcintire.evan@gmail.com>
2016-12-17 02:58:29 -0500

    Add base site to routing, add Git LFS
    * Main site now is handled through express
    * Hellish nginx issues fixed

--- webserver.js
+++ webserver.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var app = express();
+var router = express.Router();
 var fs = require('fs');
 
 var json_data = JSON.parse(fs.readFileSync('gallery_items.json', 'utf8'));
@@ -8,10 +9,18 @@ var data = json_data["images"];
 
 app.set('view engine', 'pug');
 
-app.get('/gallery/', function(req, res) {
+router.use(function timeLog (req, res, next) {
+  console.log('Time: ', Date.now());
+  next();
+});
+
+router.get('/gallery', function(req, res) {
+    console.log("Gallery request!");
     res.render('gallery', {title: 'AmiDraw Gallery', gallery_items: data});
 });
+router.use('/', express.static('public'));
+app.use('/', router);
 
 app.listen(8080, function() {
     console.log("Running!");
-})
+});
